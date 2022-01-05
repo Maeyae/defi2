aea9524e32faa879035a1884d115aca92a219c15
maeva <maeganelys@gmail.com>
2022-01-05 21:46:51 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     home: Scaffold(
+       backgroundColor: Colors.red[50],
+         body: SafeArea(
+           child: CircleAvatar(radius: 100,
+           backgroundImage: NetworkImage("https://static.actugaming.net/media/2021/12/genshin-impact-yae-miko.jpg"),
+         ),
+         ) ,
+     ),
+   );
+
+  }
+}
